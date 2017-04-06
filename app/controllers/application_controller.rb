acf873f30b0757b2b7f703fd705b6ee4d26ab919
Steven Nunez <steven.nunez@gmail.com>
2017-04-06 15:00:37 -0400

    Do the thing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
   before_action :authenticate
 
   def authenticate
@@ -6,6 +7,13 @@ class ApplicationController < ActionController::API
   end
 
   def logged_in?
-    false
+    authenticate_or_request_with_http_token do |token, options|
+      begin
+      Account.from_token(token)
+      rescue JWT::DecodeError
+        binding.pry
+        false
+      end
+    end
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,11 @@ class Account < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
 
+  def self.from_token(token)
+    account_id = Auth.decode(token)["account_id"]
+    Account.find(account_id)
+  end
+
   def self.authenticate(username, password)
     account = Account.find_by(username: username)
     account && account.authenticate(password)
